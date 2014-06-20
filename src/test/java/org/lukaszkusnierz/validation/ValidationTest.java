6d7c55544e94c80352c9712e02c92a2403dcf3aa
lukasz.kusnierz <lukasz.kusnierz@ocado.com>
2014-06-20 15:56:21 +0200

    IterableValidationEntry tests

--- src/test/java/org/lukaszkusnierz/validation/ValidationTest.java
+++ src/test/java/org/lukaszkusnierz/validation/ValidationTest.java
@@ -5,12 +5,15 @@ import org.junit.Test;
 import org.lukaszkusnierz.validation.result.Validated;
 import org.lukaszkusnierz.validation.validator.string.StringValidators;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ValidationTest {
 
 	@Test
 	public void complex_object_should_be_valid() {
 		//setup
-		final ComplexType complexType = new ComplexType( 4, "description" );
+		final ComplexType complexType = new ComplexType( 4, "description", null );
 		//examine
 		Validated<ComplexType> validationResult = Validation.of( ComplexType.class )
 				.validate( ComplexType::getNumber ).with( i -> i > 3 )
@@ -23,7 +26,7 @@ public class ValidationTest {
 	@Test
 	public void complex_object_should_be_invalid() {
 		//setup
-		final ComplexType complexType = new ComplexType( 4, "description" );
+		final ComplexType complexType = new ComplexType( 4, "description", null );
 		//examine
 		Validated<ComplexType> validationResult = Validation.of( ComplexType.class )
 				.validate( ComplexType::getNumber ).with( i -> i > 3 )
@@ -33,14 +36,52 @@ public class ValidationTest {
 		Assert.assertTrue( validationResult.isInvalid() );
 	}
 
+	@Test
+	public void null_ages_should_be_valid() {
+		//setup
+		final ComplexType complexType = new ComplexType( 4, "description", null );
+		//examine
+		Validated<ComplexType> validationResult = Validation.of( ComplexType.class )
+				.validateAll( ComplexType::getAge ).with( i -> i > 18)
+				.go( complexType );
+		//verify
+		Assert.assertTrue( validationResult.isValid() );
+	}
+
+	@Test
+	public void all_ages_should_be_valid() {
+		//setup
+		final ComplexType complexType = new ComplexType( 4, "description", Arrays.asList( 20, 30, 40 ) );
+		//examine
+		Validated<ComplexType> validationResult = Validation.of( ComplexType.class )
+				.validateAll( ComplexType::getAge ).with( i -> i > 18 )
+				.go( complexType );
+		//verify
+		Assert.assertTrue( validationResult.isValid() );
+	}
+
+	@Test
+	public void should_be_invalid_due_to_last_age() {
+		//setup
+		final ComplexType complexType = new ComplexType( 4, "description", Arrays.asList( 20, 30, 18 ) );
+		//examine
+		Validated<ComplexType> validationResult = Validation.of( ComplexType.class )
+				.validateAll( ComplexType::getAge ).with( i -> i > 18 )
+				.go( complexType );
+		//verify
+		Assert.assertTrue( validationResult.isInvalid() );
+	}
+
 	private static class ComplexType {
 
 		private final Integer number;
 		private final String text;
+		private final List<Integer> age;
 
-		private ComplexType( final Integer number, final String text ) {
+		private ComplexType( final Integer number, final String text, final List<Integer> age ) {
 			this.number = number;
 			this.text = text;
+			this.age = age;
 		}
 
 		private Integer getNumber() {
@@ -49,6 +90,10 @@ public class ValidationTest {
 
 		private String getText() {
 			return text;
+		}
+
+		public List<Integer> getAge() {
+			return age;
 		}
 	}
 }
