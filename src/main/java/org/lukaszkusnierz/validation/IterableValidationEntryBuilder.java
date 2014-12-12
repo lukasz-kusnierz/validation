ad54c02f7ba1772e4617ced1cf4bf08ef2e0a42b
lukasz.kusnierz <lukasz.kusnierz@ocado.com>
2014-12-12 09:03:33 +0100

    ValidationEntryBuilder

--- src/main/java/org/lukaszkusnierz/validation/IterableValidationEntryBuilder.java
+++ src/main/java/org/lukaszkusnierz/validation/IterableValidationEntryBuilder.java
@@ -1,6 +1,7 @@
 package org.lukaszkusnierz.validation;
 
 import org.lukaszkusnierz.validation.chain.ValidationChain;
+import org.lukaszkusnierz.validation.chain.ValidationChainBuilder;
 import org.lukaszkusnierz.validation.validator.Validator;
 
 import java.util.function.Function;
@@ -10,22 +11,29 @@ public final class IterableValidationEntryBuilder<T, FIELD, ITERABLE extends Ite
 	private final Validation<T> validation;
 	private final Function<T, ITERABLE> fieldExtractor;
 
-	public IterableValidationEntryBuilder( final Validation<T> validation, final Function<T, ITERABLE> fieldExtractor ) {
-		if ( null == validation ) {
-			throw new IllegalArgumentException( "Validation cannot be null, can it?" );
+	public IterableValidationEntryBuilder(final Validation<T> validation, final Function<T, ITERABLE> fieldExtractor) {
+		if (null == validation) {
+			throw new IllegalArgumentException("Validation cannot be null, can it?");
 		}
-		if ( null == fieldExtractor ) {
-			throw new IllegalArgumentException( "Field extractor cannot be null, use method reference syntax, ex: with( Map::keySet )" );
+		if (null == fieldExtractor) {
+			throw new IllegalArgumentException("Field extractor cannot be null, use method reference syntax, ex: with( Map::keySet )");
 		}
 		this.validation = validation;
 		this.fieldExtractor = fieldExtractor;
 	}
 
-	public Validation<T> with( final ValidationChain<FIELD> validationChain ) {
-		return this.validation.add( new IterableValidationEntry<>( validationChain, this.fieldExtractor ) );
+	public Validation<T> with(final ValidationChain<FIELD> validationChain) {
+		return this.validation.add(new IterableValidationEntry<>(validationChain, this.fieldExtractor));
 	}
 
-	public Validation<T> with( final Validator<FIELD> validator ) {
-		return this.validation.add( new IterableValidationEntry<>( ValidationChain.use( validator ), this.fieldExtractor ) );
+	public Validation<T> with(final Validator<FIELD> validator) {
+		return this.validation.add(new IterableValidationEntry<>(ValidationChain.use(validator), this.fieldExtractor));
+	}
+
+	public Validation<T> with(final ValidationChainBuilder<FIELD> validationChainBuilder) {
+		if (null == validationChainBuilder) {
+			throw new IllegalArgumentException("ValidationChainBuilder cannot be null");
+		}
+		return this.validation.add(new IterableValidationEntry<>(validationChainBuilder.chain(), this.fieldExtractor));
 	}
 }
--- src/main/java/org/lukaszkusnierz/validation/ValidationEntryBuilder.java
+++ src/main/java/org/lukaszkusnierz/validation/ValidationEntryBuilder.java
@@ -1,6 +1,7 @@
 package org.lukaszkusnierz.validation;
 
 import org.lukaszkusnierz.validation.chain.ValidationChain;
+import org.lukaszkusnierz.validation.chain.ValidationChainBuilder;
 import org.lukaszkusnierz.validation.validator.Validator;
 
 import java.util.function.Function;
@@ -10,22 +11,29 @@ public final class ValidationEntryBuilder<T, FIELD> {
 	private final Validation<T> validation;
 	private final Function<T, FIELD> fieldExtractor;
 
-	public ValidationEntryBuilder( final Validation<T> validation, final Function<T, FIELD> fieldExtractor ) {
-		if ( null == validation ) {
-			throw new IllegalArgumentException( "Validation cannot be null, can it?" );
+	public ValidationEntryBuilder(final Validation<T> validation, final Function<T, FIELD> fieldExtractor) {
+		if (null == validation) {
+			throw new IllegalArgumentException("Validation cannot be null, can it?");
 		}
-		if ( null == fieldExtractor ) {
-			throw new IllegalArgumentException( "Field extractor cannot be null, use method reference syntax, ex: with( String::length )" );
+		if (null == fieldExtractor) {
+			throw new IllegalArgumentException("Field extractor cannot be null, use method reference syntax, ex: with( String::length )");
 		}
 		this.validation = validation;
 		this.fieldExtractor = fieldExtractor;
 	}
 
-	public Validation<T> with( final ValidationChain<FIELD> validationChain ) {
-		return this.validation.add( new SimpleValidationEntry<>( validationChain, this.fieldExtractor ) );
+	public Validation<T> with(final ValidationChain<FIELD> validationChain) {
+		return this.validation.add(new SimpleValidationEntry<>(validationChain, this.fieldExtractor));
 	}
 
-	public Validation<T> with( final Validator<FIELD> validator ) {
-		return this.validation.add( new SimpleValidationEntry<>( ValidationChain.use( validator ), this.fieldExtractor ) );
+	public Validation<T> with(final Validator<FIELD> validator) {
+		return this.validation.add(new SimpleValidationEntry<>(ValidationChain.use(validator), this.fieldExtractor));
+	}
+
+	public Validation<T> with(final ValidationChainBuilder<FIELD> validationChainBuilder) {
+		if (null == validationChainBuilder) {
+			throw new IllegalArgumentException("ValidationChainBuilder cannot be null");
+		}
+		return this.validation.add(new SimpleValidationEntry<>(validationChainBuilder.chain(), this.fieldExtractor));
 	}
 }
