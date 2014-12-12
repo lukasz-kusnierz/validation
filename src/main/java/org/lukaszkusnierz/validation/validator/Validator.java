67599e1f1704aedc17ff2b54455ca3d5072713e7
lukasz.kusnierz <lukasz.kusnierz@ocado.com>
2014-12-12 09:13:17 +0100

    Validator JavaDoc

--- src/main/java/org/lukaszkusnierz/validation/validator/Validator.java
+++ src/main/java/org/lukaszkusnierz/validation/validator/Validator.java
@@ -1,6 +1,18 @@
 package org.lukaszkusnierz.validation.validator;
 
+/**
+ * Validator is a fundamental building block for validation process.
+ *
+ * @param <T> type of object under validation
+ */
 public interface Validator<T> {
 
-	boolean isValid( T subject );
+	/**
+	 * Checks if the subject is valid.
+	 * Implementations should be null-safe and should not modify the argument.
+	 *
+	 * @param subject object under validation
+	 * @return <code>true</code> if subject is valid, <code>false</code> otherwise
+	 */
+	boolean isValid(T subject);
 }
