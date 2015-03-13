1fce0bf2cdaf6dfd42e088db5965d94a348227b1
Lukasz Kusnierz <webmaster@lukaszkusnierz.org>
2015-03-13 22:06:47 +0100

    Validator.fromFunction

--- src/main/java/org/lukaszkusnierz/validation/validator/Validator.java
+++ src/main/java/org/lukaszkusnierz/validation/validator/Validator.java
@@ -1,11 +1,26 @@
 package org.lukaszkusnierz.validation.validator;
 
+import java.util.function.Function;
+
 /**
  * Validator is a fundamental building block for validation process.
  *
  * @param <T> type of object under validation
  */
 public interface Validator<T> {
+
+	public static <T> Validator<T> fromFunction( final Function<T, Boolean> function ) {
+		if ( null == function ) {
+			throw new IllegalArgumentException( "Function cannot be null" );
+		}
+		return ( subject ) -> {
+			final Boolean isValid = function.apply( subject );
+			if ( null == isValid ) {
+				throw new NullPointerException( String.format( "Your function '%s' did return null for input: '%s'. It must always return a Boolean to use it as a Validator.", function, subject ) );
+			}
+			return isValid;
+		};
+	}
 
 	/**
 	 * Checks if the subject is valid.
@@ -14,5 +29,5 @@ public interface Validator<T> {
 	 * @param subject object under validation
 	 * @return <code>true</code> if subject is valid, <code>false</code> otherwise
 	 */
-	boolean isValid(T subject);
+	boolean isValid( T subject );
 }
